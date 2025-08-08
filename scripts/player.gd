7d3880856891753d104ba2c942b803d5fe7cc94d
Lucky Adams <proximadams@gmail.com>
2025-08-08 17:49:16 -0500

    improve movement & fix bug

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,9 +3,9 @@ extends CharacterBody3D
 const GRAVITY               : float = -200.0
 const MAX_PROPELLOR_SPEED   : float = 20.0
 const MAX_PROPULSION_SPEED  : float = 10000.0
-const MOVE_ACCELERATION     : float = 100.0
+const MOVE_ACCELERATION     : float = 400.0
 const PROPELLOR_ACCELERATION: float = 50.0
-const SHOOT_DECCELERATION   : float = 5000.0
+const SHOOT_DECCELERATION   : float = 1000.0
 const TILT_DOWN_SPEED       : float = 100.0
 const WING_DEGREES          : float = 45.0
 
@@ -31,6 +31,10 @@ func _get_acceleration_decceleration_mult() -> float:
 	var result = 1.0
 	if Input.is_action_pressed('shoot'):
 		result = -1.0
+	if result < 0.0:
+		result *= SHOOT_DECCELERATION
+	else:
+		result *= MOVE_ACCELERATION
 	return result
 
 func handle_gravity() -> void:
@@ -38,23 +42,26 @@ func handle_gravity() -> void:
 
 func handle_movement(delta: float) -> void:
 	var accdeccMult = _get_acceleration_decceleration_mult()
-	propulsionSpeed = max(0.0, propulsionSpeed + (accdeccMult * delta * MOVE_ACCELERATION))
+	propulsionSpeed = max(0.0, propulsionSpeed + (accdeccMult * delta))
 	propulsionSpeed -= (global_basis.z.normalized().y - 0.5) * delta * 70.0
-	propulsionSpeed = min(MAX_PROPULSION_SPEED, propulsionSpeed)
+	propulsionSpeed = min(MAX_PROPULSION_SPEED, max(0.0, propulsionSpeed))
 
 	velocity = Vector3(propulsionSpeed, propulsionSpeed, propulsionSpeed)
 	velocity *= delta * global_basis.z.normalized()
 	velocity.y += delta * fallSpeed
 	move_and_slide()
+	transform = transform.orthonormalized()
 
 func handle_rotation(delta: float) -> void:
-	var speedMultX = 0.3 + (0.003 * (sqrt(propulsionSpeed * abs(fallSpeed))))
+	var speedMultX = 0.5 + (0.003 * (sqrt(propulsionSpeed * abs(fallSpeed))))
 	var speedMultY = 0.4 + (0.0008 * (sqrt(propulsionSpeed * abs(fallSpeed))))
 	var inputDirection = get_input_direction()
 	if 0.0 < propulsionSpeed or rad_to_deg(rotation.x) < 80.0 or 100.0 < rad_to_deg(rotation.x):
 		rotate_object_local(Vector3.RIGHT, delta * inputDirection.y * speedMultY)
 	rotate_object_local(Vector3.FORWARD, delta * inputDirection.x * speedMultX)
 	if rad_to_deg(rotation.x) < 80.0 or 100.0 < rad_to_deg(rotation.x):
+		if propulsionSpeed == 0.0:
+			propulsionSpeed = 0.001
 		rotation.x += delta * max(0.0, max(0.0, TILT_DOWN_SPEED/propulsionSpeed) - 0.1)
 
 func handle_spin_propellor(delta: float) -> void:
