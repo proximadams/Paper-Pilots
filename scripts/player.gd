f249a29870a5300a3aad0b75bb1cacc1fcb3acb8
Lucky Adams <proximadams@gmail.com>
2025-08-07 20:13:19 -0500

    accelerate and deccelerate

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,37 @@
 extends CharacterBody3D
 
-const WING_DEGREES = 45.0
+const ACCELERATION       : float = 100.0
+const MAX_PROPELLOR_SPEED: float = 20.0
+const SHOOT_DECCELERATION: float = 300.0
+const WING_DEGREES       : float = 45.0
 
 @export var leftMoveableWing : Node3D
 @export var rightMoveableWing: Node3D
 
 @export var propellor: MeshInstance3D
 
+var moveSpeed      : float = 0.0
+var propellorSpeed : float = 0.0
+
 func _process(delta: float) -> void:
 	handle_input_wing_angles()
 	handle_spin_propellor(delta)
 	handle_rotation(delta)
+	handle_movement(delta)
+
+func _get_acceleration_decceleration_mult() -> float:
+	var result = 1.0
+	if Input.is_action_pressed('shoot'):
+		result = -1.0
+	return result
+
+func handle_movement(delta: float) -> void:
+	var accdeccMult = _get_acceleration_decceleration_mult()
+	moveSpeed = max(0.0, moveSpeed + (accdeccMult * delta * ACCELERATION))
+
+	velocity = Vector3(moveSpeed, moveSpeed, moveSpeed)
+	velocity *= delta * global_basis.z.normalized()
+	move_and_slide()
 
 func handle_rotation(delta: float) -> void:
 	var inputDirection = get_input_direction()
@@ -18,7 +39,9 @@ func handle_rotation(delta: float) -> void:
 	rotate_object_local(Vector3.FORWARD, delta * inputDirection.x)
 
 func handle_spin_propellor(delta: float) -> void:
-	propellor.rotation.z += delta * 10.0
+	var accdeccMult = _get_acceleration_decceleration_mult()
+	propellorSpeed = max(0.0, min(MAX_PROPELLOR_SPEED, propellorSpeed + (accdeccMult * delta * ACCELERATION)))
+	propellor.rotation.z += delta * propellorSpeed
 
 func handle_input_wing_angles() -> void:
 	var inputDirection = get_input_direction()
