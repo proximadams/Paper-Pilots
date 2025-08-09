d96447c941fac1bbcac60aee6b98bd1d4199de16
Lucky Adams <proximadams@gmail.com>
2025-08-09 10:34:29 -0500

    camera uses physics process

--- scripts/player_camera.gd
+++ scripts/player_camera.gd
@@ -3,6 +3,6 @@ extends Camera3D
 @export var followTarget: Node3D
 @export var lookAtTarget: Node3D
 
-func _process(_delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	global_position = lerp(global_position, followTarget.global_position, 0.05)
 	look_at(lookAtTarget.global_position)
