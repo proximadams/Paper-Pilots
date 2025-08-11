8a90014ab46ea24a3d60a9af9007f1e386e84984
Lucky Adams <proximadams@gmail.com>
2025-08-10 20:30:20 -0500

    better skybox

---
+++ scenes/physical_world_elements_follow.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+@export var playerArr: Array[CharacterBody3D]
+
+func _process(_delta: float) -> void:
+	var bottomY = 0.0
+	for currPlayer in playerArr:
+		if currPlayer.global_position.y < bottomY:
+			bottomY = currPlayer.global_position.y
+	global_position.y = bottomY - 1000.0
