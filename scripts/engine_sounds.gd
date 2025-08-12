b0fcbb308dc5a1270be8faf346e5a44db71dc4c7
Lucky Adams <proximadams@gmail.com>
2025-08-12 13:58:47 -0500

    remove warning

--- scripts/engine_sounds.gd
+++ scripts/engine_sounds.gd
@@ -7,7 +7,6 @@ var pitchTarget = 1.0
 
 func _process(_delta: float):
 	var direction = player1.global_position - player2.global_position
-	var distance = direction.length()
 	direction = direction.normalized()
 	var relative_velocity = player1.velocity - player2.velocity
 	var dot = direction.dot(relative_velocity)
