0de7092898b75b625729cc0ac7662d856459790d
Lucky Adams <proximadams@gmail.com>
2025-08-13 09:20:28 -0500

    engine volume changes

--- scripts/engine_sounds.gd
+++ scripts/engine_sounds.gd
@@ -7,8 +7,10 @@ var pitchTarget = 1.0
 
 func _process(_delta: float):
 	var direction = player1.global_position - player2.global_position
+	var distance = direction.length()
 	direction = direction.normalized()
 	var relative_velocity = player1.velocity - player2.velocity
 	var dot = direction.dot(relative_velocity)
 	pitchTarget = 1.0 - clamp(0.002 * dot, -0.3, 0.3)
 	pitch_scale = lerp(pitch_scale, pitchTarget, 0.02)
+	volume_db = clamp(5.0 - 0.1 * distance, -10.0, 5.0)
