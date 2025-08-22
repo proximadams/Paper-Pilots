2a7f4e2392607b6d98ea6c72aa977b42f1defe2f
Lucky Adams <proximadams@gmail.com>
2025-08-22 15:15:17 -0500

    attempt loading screen changes

--- scripts/loading_screen.gd
+++ scripts/loading_screen.gd
@@ -7,8 +7,8 @@ var assetIndex := 0
 var nextScene  := ''
 var previousInst = null
 
-@onready var assetHolder  : Control = $AssetHolder
-@onready var origSfxVolume: float   = Global.settingsData.sfxVolume
+@onready var assetHolder  : Node3D = $AssetHolder
+@onready var origSfxVolume: float  = Global.settingsData.sfxVolume
 
 func _process(_delta: float) -> void:
 	if nextScene != '':
@@ -22,11 +22,14 @@ func _process(_delta: float) -> void:
 				previousInst.free()
 				previousInst = null
 		else:
-			assetIndex = 0
-			visible = false
 			Global.settingsData.sfxVolume = origSfxVolume
 			get_tree().change_scene_to_file(nextScene)
 			nextScene = ''
+	elif assetIndex != 0:
+		assetIndex = 0
+		visible = false
+		for currChild in assetHolder.get_children():
+			currChild.queue_free()
 
 func load(scenePathGiven: String) -> void:
 	nextScene = scenePathGiven
